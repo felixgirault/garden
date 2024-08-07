7550fbd2ab6a8ca62fca3933d92c56fdb08b6603
Félix Girault <felix.girault@gmail.com>
2024-08-08 01:50:38 +0200

    Genericized flattening of collections

---
+++ src/services/collections.ts
@@ -0,0 +1,24 @@
+import {
+	type CollectionEntry,
+	type CollectionKey,
+	getCollection
+} from 'astro:content';
+
+export type FlatEntry<C extends CollectionKey> =
+	CollectionEntry<C>['data'] & {
+		id: CollectionEntry<C>['id'];
+	};
+
+const flattenEntry = async <C extends CollectionKey>(
+	entry: CollectionEntry<C>
+): Promise<FlatEntry<C>> => ({
+	...entry.data,
+	id: entry.id
+});
+
+const flatCollection = <C extends CollectionKey>(type: C) =>
+	getCollection(type).then((entries) =>
+		Promise.all(entries.map(flattenEntry))
+	);
+
+export const albumCollection = await flatCollection('albums');
