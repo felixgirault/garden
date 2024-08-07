d87912fdb32b0c45dd22303785fef069f062abf0
Félix Girault <felix.girault@gmail.com>
2024-08-08 01:35:34 +0200

    Using Grapes theme for code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import {defineConfig} from 'astro/config';
 
+const theme = await fetch(
+	'https://raw.githubusercontent.com/felixgirault/grapes-theme/master/themes/Grapes-color-theme.json'
+);
+
 // https://astro.build/config
 export default defineConfig({
 	markdown: {
 		shikiConfig: {
-			theme: 'material-theme-ocean'
+			theme: await theme.json()
 		}
 	}
 });
