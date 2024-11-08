e755bbd05363401fa51a61fcbd00896587780076
Félix Girault <felix.girault@gmail.com>
2024-11-08 15:45:33 +0100

    Added font fallbacks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import {defineConfig} from 'astro/config';
+import {FontaineTransform} from 'fontaine';
 
 const theme = await fetch(
 	'https://raw.githubusercontent.com/felixgirault/grapes-theme/master/themes/Grapes-color-theme.json'
@@ -10,5 +11,14 @@ export default defineConfig({
 		shikiConfig: {
 			theme: await theme.json()
 		}
+	},
+	vite: {
+		plugins: [
+			FontaineTransform.vite({
+				fallbacks: ['verdana'],
+				resolvePath: (id) =>
+					new URL(`./public${id}`, import.meta.url)
+			})
+		]
 	}
 });
