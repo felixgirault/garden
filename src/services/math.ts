c8a049da1689fabdaffe0c2f06a426beca56b7c9
Félix Girault <felix.girault@gmail.com>
2024-11-09 11:12:37 +0100

    Using routes as seeds to randomize backgrounds

--- src/services/math.ts
+++ src/services/math.ts
@@ -3,3 +3,21 @@ export const round = (exp: number, places = 4) => {
 	exp = Math.round(Number(`${exp}e+${places}`));
 	return Number(`${exp}e-${places}`);
 };
+
+// @see https://stackoverflow.com/a/7616484/2391359
+export const hashCode = (string: string) => {
+	let hash = 0;
+	let char;
+
+	if (!string.length) {
+		return hash;
+	}
+
+	for (let i = 0; i < string.length; i++) {
+		char = string.charCodeAt(i);
+		hash = (hash << 5) - hash + char;
+		hash |= 0;
+	}
+
+	return hash;
+};
