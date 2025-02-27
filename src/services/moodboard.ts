4575699a7dbf4e58395aa01a47252f9c1e524822
Félix Girault <felix.girault@gmail.com>
2025-02-27 02:07:55 +0100

    Split moodboard into web components

--- src/services/moodboard.ts
+++ src/services/moodboard.ts
@@ -1,5 +1,7 @@
+import kdTree from 'kd-tree-javascript';
+import {type Point, interpolateSpline} from './2d';
 import {type DbData, DbTrackAttributesMax} from './db';
-import {round} from './math';
+import {randomIndices, round} from './math';
 import type {SpotifyId} from './spotify';
 
 export type MoodboardTrack = {
@@ -26,14 +28,85 @@ export const fromDbTracks = (data: DbData): MoodboardTrack[] => {
 			title: track.title,
 			artist: artists[track.artistId].name,
 			energy: round(
-				(track.attributes.energy / DbTrackAttributesMax) *
-					100,
-				2
+				track.attributes.energy / DbTrackAttributesMax,
+				3
 			),
 			valence: round(
-				(track.attributes.valence / DbTrackAttributesMax) *
-					100,
-				2
+				track.attributes.valence / DbTrackAttributesMax,
+				3
 			)
 		}));
 };
+
+export class MoodboardTrackSpace {
+	#tracks: MoodboardTrack[];
+	#tree: kdTree.kdTree<MoodboardTrack>;
+
+	constructor(tracks: MoodboardTrack[]) {
+		this.#tracks = tracks;
+		this.#tree = new kdTree.kdTree(
+			tracks,
+			(a, b) =>
+				(a.valence - b.valence) ** 2 +
+				(a.energy - b.energy) ** 2,
+			['valence', 'energy']
+		);
+	}
+
+	searchAlongPath(path: Point[], limit = 50) {
+		const points = interpolateSpline(path, limit - 1);
+
+		// Instead of searching for many neighbors everytime,
+		// a small search is made first, which should cover
+		// almost every case. If there is not enough results,
+		// the search is made again with a greater search
+		// radius. Then rinse and repeat.
+		const neighbors = points.map(function* (
+			this: MoodboardTrackSpace,
+			[valence, energy]
+		) {
+			let count = 3;
+			let lowerBound = 0;
+
+			while (count < this.#tracks.length) {
+				const nearest = this.#tree
+					.nearest(
+						{
+							valence,
+							energy
+						} as MoodboardTrack,
+						count
+					)
+					.filter(([_, d]) => d > lowerBound)
+					.toSorted((a, b) => a[1] - b[1]);
+
+				for (const [track] of nearest) {
+					yield track;
+				}
+
+				const farthest = nearest.at(-1);
+				lowerBound = farthest ? farthest[1] : lowerBound;
+				count *= 3;
+			}
+		}, this);
+
+		const used = new WeakSet<MoodboardTrack>();
+		const nearestTracks: MoodboardTrack[] = [];
+
+		// Traversing neighbors in a random order avoids
+		// accumulating errors to the end of the list.
+		for (const i of randomIndices(neighbors.length)) {
+			const next = neighbors[i];
+
+			for (const track of next) {
+				if (!used.has(track)) {
+					used.add(track);
+					nearestTracks[i] = track;
+					break;
+				}
+			}
+		}
+
+		return nearestTracks;
+	}
+}
